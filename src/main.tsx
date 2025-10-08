a9567c46615eeba33ed68687cd9940a211503a0c
test-lexion <hyamamoto334@gmail.com>
2025-10-08 08:37:49 +0000

    gaem

--- src/main.tsx
+++ src/main.tsx
@@ -8,3 +8,19 @@ createRoot(document.getElementById('root')!).render(
     <App />
   </StrictMode>,
 )
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import "./config"; // Import config to initialize appKit
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </StrictMode>
+);
